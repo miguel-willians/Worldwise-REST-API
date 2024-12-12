4b66f12a579ce7fd24e03f06b14566a488cfd55a
miguel-willians <miiguel.wso@gmail.com>
2024-12-12 18:46:02 -0300

    Configurando a dependência Dotenv

--- src/main/java/com/worldwise/cities/config/CorsConfig.java
+++ src/main/java/com/worldwise/cities/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.worldwise.cities;
+package com.worldwise.cities.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
---
+++ src/main/java/com/worldwise/cities/config/DotenvConfig.java
@@ -0,0 +1,27 @@
+package com.worldwise.cities.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+// import javax.annotation.PostConstruct;
+// import java.util.Map;
+
+@Configuration
+public class DotenvConfig {
+
+    @Value("${DOTENV_FILE:.env}") // Permitir especificar um arquivo diferente se necessário
+    private String dotenvFile;
+
+    @jakarta.annotation.PostConstruct
+    public void loadDotenv() {
+        Dotenv dotenv = Dotenv.configure()
+                .filename(dotenvFile)
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
+}
+
