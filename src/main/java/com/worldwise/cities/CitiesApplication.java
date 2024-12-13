1ef173edd3031438a7f6c9c811ed6a3585fe4e10
miguel-willians <miiguel.wso@gmail.com>
2024-12-13 15:33:39 -0300

    Removendo o DotEnv

--- src/main/java/com/worldwise/cities/CitiesApplication.java
+++ src/main/java/com/worldwise/cities/CitiesApplication.java
@@ -3,18 +3,11 @@ package com.worldwise.cities;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class CitiesApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-        System.setProperty("DOTENV_DB_HOST", dotenv.get("DOTENV_DB_HOST"));
-        System.setProperty("DOTENV_DB_PORT", dotenv.get("DOTENV_DB_PORT"));
-        System.setProperty("DOTENV_DB_NAME", dotenv.get("DOTENV_DB_NAME"));
-        System.setProperty("DOTENV_DB_USER", dotenv.get("DOTENV_DB_USER"));
-        System.setProperty("DOTENV_DB_PASSWORD", dotenv.get("DOTENV_DB_PASSWORD"));
 
 		SpringApplication.run(CitiesApplication.class, args);
 	}
